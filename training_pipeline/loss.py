7052f524558180d5fe0af1a3c6c112e5e9d3a29d
pelayoelvira <UO288736@uniovi.es>
2025-04-24 19:45:50 +0000

    Refactored metric names and remove unused function

--- training_pipeline/loss.py
+++ training_pipeline/loss.py
@@ -3,14 +3,6 @@ import tensorflow as tf
 from keras.saving import register_keras_serializable
 
 
-def dice_coef(y_true, y_pred, smooth=1e-6):
-    y_true_f = tf.reshape(y_true, [-1])  # Aplanar
-    y_pred_f = tf.reshape(y_pred, [-1])  # Aplanar
-    intersection = tf.reduce_sum(y_true_f * y_pred_f)
-    return (2. * intersection + smooth) / (tf.reduce_sum(y_true_f) + tf.reduce_sum(y_pred_f) + smooth)
-
-
-
 def precision(y_true, y_pred, smooth=1e-6):
     y_true_f = tf.reshape(y_true, [-1])
     y_pred_f = tf.reshape(y_pred, [-1])
